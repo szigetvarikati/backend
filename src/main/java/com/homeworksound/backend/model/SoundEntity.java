79e84725a36c9892ffa19d1f3aae8dfcece6f450
szigetvarikati <szigetvarikati@gmail.com>
2023-12-06 21:50:22 +0100

    create SoundEntity class

---
+++ src/main/java/com/homeworksound/backend/model/SoundEntity.java
@@ -0,0 +1,45 @@
+package com.homeworksound.backend.model;
+
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.time.LocalDateTime;
+
+@Entity
+@Table(name = "sound")
+@Getter
+@Setter
+public class SoundEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer soundId;
+
+    @Column(nullable = false)
+    private String soundName;
+
+    @Lob
+    //@Column( nullable = false)
+    private byte[] soundData;
+
+    @Column(nullable = false)
+    private String soundExtension;
+
+    @Column( nullable = false)
+    private LocalDateTime soundCreatedDatetime;
+
+    private LocalDateTime soundUpdatedDatetime;
+
+    public SoundEntity(Integer soundId, String soundName, byte[] soundData, String soundExtension) {
+
+        this.soundName = soundName;
+        this.soundData = soundData;
+        this.soundExtension = soundExtension;
+        this.soundCreatedDatetime = LocalDateTime.now();
+    }
+
+    public SoundEntity() {
+    }
+}
